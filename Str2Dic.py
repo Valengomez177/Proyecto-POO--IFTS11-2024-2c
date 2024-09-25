2a97d111e68d8ad8452c9ffc497b73a3611229f4
Valentina Gómez Pereyra <valengomezp177@gmail.com>
2024-09-24 21:35:27 -0300

    Create Str2Dic.py

---
+++ Str2Dic.py
@@ -0,0 +1,23 @@
+import os
+os.system
+
+schema= "Nombre, Apellido, Edad, Email"
+row= "Valentina, Gomez Pereyra, 22, Valengomezp177@gmai.com"
+
+class Str2Dic():
+    def __init__(self, schema, separator=","):
+        self.schema = schema.split(separator)
+        self.separator = separator
+    def convert(self,row):
+        tmp = row.split(self.separator)
+        if len(tmp) == len(self.schema):
+            i = 0
+            d = {}
+            while i < len(tmp):
+                d[self.schema[i]] = tmp[i]
+                i += 1
+            return d
+o = Str2Dic(schema)
+d = o.convert(row)
+
+print(d)
